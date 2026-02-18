c5615163d1138f5ccada68ce904103fb9f853869
Vishal Bhati <vishalbhati8368@gmail.com>
2026-02-19 00:10:49 +0530

    save works

--- Library/books/urls.py
+++ Library/books/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import library_view
+from .views import library_view, search_view , save_book_view
 
 urlpatterns = [
     path("library/", library_view, name="library"),
+    path("search/", search_view, name="search"),
+    path("save/", save_book_view, name="save-book"),
 ]
--- Library/books/views.py
+++ Library/books/views.py
@@ -1,5 +1,86 @@
-from django.shortcuts import render
-from rest_framework.permissions import IsAuthenticated
+import requests
+from django.shortcuts import render, redirect
+
+import json
 
 def library_view(request):
-    return render(request, "books/library.html")
+    access = request.COOKIES.get("access")
+
+    if not access:
+        return redirect("login")
+
+    res = requests.get(
+        "http://127.0.0.1:8000/api/books/library/",
+        cookies={"access": access}, 
+    )
+
+    data = res.json() if res.status_code == 200 else {}
+
+    return render(
+        request,
+        "books/library.html",
+        {
+            "reading": data.get("reading", []),
+            "finished": data.get("finished", []),
+        },
+    )
+
+def search_view(request):
+    access = request.COOKIES.get("access")
+
+    if not access:
+        return redirect("login")
+
+    query = request.GET.get("q")
+    results = []
+
+    if query:
+        res = requests.get(
+            "http://127.0.0.1:8000/api/books/search/",
+            params={"q": query},
+            cookies={"access": access},
+        )
+
+        if res.status_code == 200:
+            data = res.json()
+            results = data.get("items", [])
+
+    return render(
+        request,
+        "books/search.html",
+        {
+            "results": results,
+            "query": query,
+        },
+    )
+
+
+def save_book_view(request):
+    if request.method != "POST":
+        return redirect("search")
+
+    access = request.COOKIES.get("access")
+    if not access:
+        return redirect("login")
+
+    google_book_id = request.POST.get("google_book_id")
+    if not google_book_id:
+        return redirect("search")
+
+    # Fetch book again via API (clean & safe)
+    res = requests.get(
+        "https://www.googleapis.com/books/v1/volumes/" + google_book_id
+    )
+
+    if res.status_code != 200:
+        return redirect("search")
+
+    item = res.json()
+
+    requests.post(
+        "http://127.0.0.1:8000/api/books/save/",
+        json={"item": item},
+        cookies={"access": access},
+    )
+
+    return redirect("library")
